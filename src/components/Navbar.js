93668f0b528697ba883e55148e7791881b595af5
Harsh <harsh0564.cse19@chitkara.edu.in>
2022-05-12 03:14:24 +0530

    mobile-nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+import logo from "../images/logo.svg";
+
+import menu from "../images/icon-menu.svg";
+import closeMenu from "../images/icon-close-menu.svg";
+import downArrow from "../images/icon-arrow-down.svg";
+import upArrow from "../images/icon-arrow-up.svg";
+import todo from "../images/icon-todo.svg";
+import calender from "../images/icon-calendar.svg";
+import reminder from "../images/icon-reminders.svg";
+import planning from "../images/icon-planning.svg";
+
+export default function Navbar() {
+  const [showMenu, setMenu] = useState(false);
+
+  const [dropdown, setDropdown] = useState([false, false]);
+
+  return (
+    <div id="nav">
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className="hamburger-menu">
+        <img src={menu} alt="menu" onClick={() => setMenu(true)} />
+      </div>
+
+      <div className={showMenu ? "hidden-menu" : "hidden"}>
+        <div style={{ marginLeft: "auto" }}>
+          <img onClick={() => setMenu(false)} src={closeMenu} alt="menu" />
+        </div>
+        <ul className="menu-list">
+          <li
+            onClick={() => {
+              let newState = !dropdown[0];
+              console.log(newState);
+              setDropdown([newState, dropdown[1]]);
+            }}
+          >
+            Features &nbsp;{"    "}
+            {dropdown[0] ? (
+              <img src={upArrow} alt="menu" onClick={() => setMenu(true)} />
+            ) : (
+              <img src={downArrow} alt="menu" onClick={() => setMenu(true)} />
+            )}
+          </li>
+
+          <div className={dropdown[0] ? "menu-dropdown" : "none"}>
+          <div className="menu-dropdown-item">
+                <img src={todo}/> <li>Todo List</li>
+            </div>
+            <div  className="menu-dropdown-item">
+                <img src={calender}/> <li>Calender</li>
+            </div>
+            <div className="menu-dropdown-item">
+                <img src={reminder}/> <li>Reminders</li>
+            </div>
+            <div className="menu-dropdown-item">
+                <img src={planning}/> <li>Planning</li>
+            </div>
+          </div>
+
+          <li
+            onClick={() => {
+              let newState = !dropdown[1];
+              console.log(newState);
+              setDropdown([dropdown[0],newState ]);
+            }}
+          >
+            Company &nbsp;{"    "}
+            {dropdown[1] ? (
+              <img src={upArrow} alt="menu" onClick={() => setMenu(true)} />
+            ) : (
+              <img src={downArrow} alt="menu" onClick={() => setMenu(true)} />
+            )}
+          </li>
+
+          <div className={dropdown[1] ? "menu-dropdown" : "none"}>
+          <li>History</li>
+          <li>Our Team</li>
+          <li>Blog</li>
+          </div>
+          <li>Careers</li>
+
+          <li>About</li>
+        </ul>
+
+        <div className="menu-btns">
+          <div className="login-btn">
+              Login
+          </div>
+
+          <div className="register-btn">
+              Register
+          </div>
+        </div>
+      </div>
+
+      <div className={showMenu ? "backdrop-blur" : "hidden"}></div>
+    </div>
+  );
+}
